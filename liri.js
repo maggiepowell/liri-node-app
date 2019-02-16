fb64de56bbba93243b8a61bac5d9bc799e2b9aeb
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-02-15 18:33:09 -0800

    omdb section updated

--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
+//add axios npm package
+var axios = require("axios");
+
 require("dotenv").config();
 
   var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-//add axios npm package
-var axios = require("axios");
 
 var artist = 
 // We then run the request with axios module on a URL with a JSON
@@ -22,9 +23,9 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
 
 // Here we incorporate the "axios" npm package
 var axios = require("axios");
-
+var song =
 // We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+axios.get("spotify" + song).then(
   function(response) {
     // Then we print out the imdbRating
     console.log("Artist: " + response.data.imdbRating);
@@ -38,18 +39,30 @@ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tr
 
 
 
-// We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    // Then we print out the following:
-    console.log("Title: " + response.data.imdbRating);
-    console.log("Year of release: " + response.data.imdbRating);
-    console.log("IMDB rating: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes rating: " + response.data.imdbRating);
-    console.log("Country where produced: " + response.data.imdbRating);
-    console.log("Language: " + response.data.imdbRating);
-    console.log("Plot: " + response.data.imdbRating);
-    console.log("Cast: " + response.data.imdbRating);
+// OMDB run the request with axios module on a URL with a JSON
+var OMDB = function(){
+    //findMovie takes in name of movie and searches the OMDB API
+this.findMovie = function (movie) {
+    var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+axios.get(URL).then(function(response) {
+    //place response.data into a variable jsonData
+    var jsonData = response.data;
+
+    //movieData = the string containint the movie data we will print to console
+    var movieData = [
+        "Title:" + jsonData.Title,
+        "Year of release:" + jsonData.Year,
+        "IMDB rating: " + jsonData.imdbRating,
+        "Rotten Tomatoes rating: " + jsonData.Ratings[1].Source,
+        "Country where produced: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Cast: " + jsonData.Actors
+    ].join("\n\n");
+
+    //print movieData to console
+    console.log(movieData)
   }
 );
-
+}
+}
