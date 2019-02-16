e5b187e6fbc9c0a12e1169999441a016498ddc12
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-02-15 19:32:28 -0800

    bands in town and if statements

--- liri.js
+++ liri.js
@@ -3,66 +3,102 @@ var axios = require("axios");
 
 require("dotenv").config();
 
-  var keys = require("./keys.js");
+var keys = require("./keys.js");
+
+// Grab search command line argument
+var search = process.argv[2];
+// Joining the remaining arguments since an actor or tv show name may contain spaces
+var term = process.argv.slice(3).join(" ");
+
+// By default, if no search type is provided, search for Mr. Nobody
+if (!search) {
+    search = "Mr. Nobody";
+}
+
+// By default, if no search term is provided, search for "Mr. Nobody"
+if (!term) {
+    term = "Mr. Nobody";
+}
+
+// Print whether searching for a show or actor, print the term as well
+if (search === "concert-this") {
+    console.log("Searching for concert");
+    bandsInTown(term);
+} else if (search === "spotify-this-song") {
+    console.log("Searching for song");
+    tv.findShow(term);
+} else if (search === "movie-this") {
+    console.log("Searching for movie");
+    OMDB(term);
+}
+
+
+
+var bandsInTown = function () {
+    //findConcert takes in name of band and searches the bands in town API
+    this.findConcert = function () {
+        var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+        // We then run the request with axios module on a URL with a JSON
+        axios.get(URL).then(function (response) {
+            // place response.data into a variable jsonData
+            var jsonData = response.data;
+
+            //concertData = the string containing the concert data we will print to console
+            var concertData = [
+                "The venue is: " + jsonData.venue.name,
+                "The venue location is: " + jsonData.venue.city,
+                "The date of event is: " + jsonData.datetime
+            ].join("\n\n");
+        }
+        );
+    }
+}
+
+// OMDB 
+var OMDB = function () {
+    //findMovie takes in name of movie and searches the OMDB API
+    this.findMovie = function () {
+        var URL = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
+        //run the request with axios module on a URL with a JSON
+        axios.get(URL).then(function (response) {
+            //place response.data into a variable jsonData
+            var jsonData = response.data;
+
+            //movieData = the string containing the movie data we will print to console
+            var movieData = [
+                "Title:" + jsonData.Title,
+                "Year of release:" + jsonData.Year,
+                "IMDB rating: " + jsonData.imdbRating,
+                "Rotten Tomatoes rating: " + jsonData.Ratings[1].Source,
+                "Country where produced: " + jsonData.Country,
+                "Language: " + jsonData.Language,
+                "Plot: " + jsonData.Plot,
+                "Cast: " + jsonData.Actors
+            ].join("\n\n");
+
+            //print movieData to console
+            console.log(movieData)
+        }
+        );
+    }
+}
+
 
 var spotify = new Spotify(keys.spotify);
 
-
-var artist = 
-// We then run the request with axios module on a URL with a JSON
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    // Then we print out the name of venue, venue location, date of event
-    console.log("The venue is: " + response.data.imdbRating);
-    console.log("The venue location is: " + response.data.imdbRating);
-    console.log("The date of event is: " + response.data.imdbRating);
-  }
-);
-
-
-
-// Here we incorporate the "axios" npm package
+// SPOTIFY incorporate the "axios" npm package
 var axios = require("axios");
 var song =
-// We then run the request with axios module on a URL with a JSON
-axios.get("spotify" + song).then(
-  function(response) {
-    // Then we print out the imdbRating
-    console.log("Artist: " + response.data.imdbRating);
-    console.log("Song name: " + response.data.imdbRating);
-    console.log("Preview link: " + response.data.imdbRating);
-    console.log("Album: " + response.data.imdbRating);
-//if no song provided program will play "The Sign" by Ace of Base
-  }
-);
+    // We then run the request with axios module on a URL with a JSON
+    axios.get("spotify" + song).then(
+        function (response) {
+            // Then we print out the imdbRating
+            console.log("Artist: " + response.data.imdbRating);
+            console.log("Song name: " + response.data.imdbRating);
+            console.log("Preview link: " + response.data.imdbRating);
+            console.log("Album: " + response.data.imdbRating);
+            //if no song provided program will play "The Sign" by Ace of Base
+        }
+    );
 
 
-
-
-// OMDB run the request with axios module on a URL with a JSON
-var OMDB = function(){
-    //findMovie takes in name of movie and searches the OMDB API
-this.findMovie = function (movie) {
-    var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-axios.get(URL).then(function(response) {
-    //place response.data into a variable jsonData
-    var jsonData = response.data;
-
-    //movieData = the string containint the movie data we will print to console
-    var movieData = [
-        "Title:" + jsonData.Title,
-        "Year of release:" + jsonData.Year,
-        "IMDB rating: " + jsonData.imdbRating,
-        "Rotten Tomatoes rating: " + jsonData.Ratings[1].Source,
-        "Country where produced: " + jsonData.Country,
-        "Language: " + jsonData.Language,
-        "Plot: " + jsonData.Plot,
-        "Cast: " + jsonData.Actors
-    ].join("\n\n");
-
-    //print movieData to console
-    console.log(movieData)
-  }
-);
-}
-}
